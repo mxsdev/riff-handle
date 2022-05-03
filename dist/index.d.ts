52bd79057c5c4f38192066ef120e5d5fcb9d2c4c
charburgx <charburgx@gmail.com>
2022-05-03 10:41:21 -0500

    Export types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,3 +4,4 @@ export { ChunkReader } from './chunkReader';
 export { RIFFReader } from './riffReader';
 export { ParseWave, parseChunkHeader } from './parseChunk';
 export { WaveWriter, RIFFWriter, ChunkWriter } from './riffWriter';
+export { WaveMeta, WaveFormat, WaveCue, WaveCues, WaveFact } from './types/chunks';
